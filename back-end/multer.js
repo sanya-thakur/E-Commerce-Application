2a53c4b9ab0e2d9636211e3976056f13f0c9408c
kalvium <sanya.thakur@kalvium.community>
2025-01-29 14:25:44 +0530

    M10

--- back-end/multer.js
+++ back-end/multer.js
@@ -1,12 +1,28 @@
-const multer = require('multer');// Configure multer storage
+const multer = require('multer');
+
+
+// Configure multer storage
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'uploads/'); // Define your upload folder
-    },
-    filename: function(req, file, cb) {
-      const uniqueSuffix =  Date.now() + '-' + Math.round.apply(Math.random() * 1e9);
-       // Define a unique filename
-       const filename = file.originalname.split(".")[0];
-       cb(null,filename + "-" + uniqueSuffix + ".png"); // Define
-    },
-  });  exports.upload = multer({ storage: storage });
+  destination: 'uploads/',
+  filename: function (req, file, cb) {
+    console.log(req.body);
+    const uniqueSuffix = Date.now() + '-' + Math.round.apply(Math.random() * 1e9);
+    // Define a unique filename
+    const filename = file.originalname.split(".")[0];
+    cb(null, filename + "-" + uniqueSuffix + ".png"); // Define
+  },
+});
+
+const pstorage = multer.diskStorage({
+  destination: 'products/',
+  filename: function (req, file, cb) {
+    console.log(req.body);
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    const filename = file.originalname.split(".")[0];
+    cb(null, filename + "-" + uniqueSuffix + ".png"); // Define
+  },
+});
+
+// Initialize upload object
+exports.upload = multer({ storage: storage });
+exports.pupload = multer({ storage: pstorage});
