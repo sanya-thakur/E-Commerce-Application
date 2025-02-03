fe81f5a6b2a7495f6e4c50aed09e8da74b10ebc8
kalvium <sanya.thakur@kalvium.community>
2025-02-03 12:24:10 +0530

    M 12

--- front-end/src/Routes.jsx
+++ front-end/src/Routes.jsx
@@ -1,5 +1,7 @@
-import LoginPage from "./pages/Login";
-import SignupPage from "./pages/Signup";
+
+import LoginPage from './pages/Login'
+import SignupPage from './pages/Signup'
 import Home from "./pages/Home";
 import CreateProduct from './pages/createProduct.jsx';
-export {LoginPage,SignupPage,Home,CreateProduct};
+import MyProducts from "./pages/myProducts.jsx";
+export {LoginPage,SignupPage,Home,CreateProduct,MyProducts};
